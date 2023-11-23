21b1be4cca802594a7e304ad70499342b15077d5
thedivtagguy <amanbhargava2001@gmail.com>
2023-11-23 20:22:56 +0530

    fix: Fix project structure

---
+++ 2023/week-47-nov-2023-r-ladies/website/src/lib/components/Globe.svelte
@@ -0,0 +1,120 @@
+<script lang="ts">
+	import { cubicOut } from 'svelte/easing';
+	import { spring } from 'svelte/motion';
+	import { geoOrthographic, geoCentroid, geoDistance } from 'd3-geo';
+	import { feature } from 'topojson-client';
+	import { Chart, Svg, GeoPath, Graticule, Tooltip, Zoom, GeoPoint } from 'layerchart';
+	import { cls, sortFunc, Button, scrollIntoView } from 'svelte-ux';
+	import { scaleSqrt, max, scaleLinear } from 'd3';
+	import data from '$lib/data/countries-110m.json';
+	import meetupData from '$lib/data/meetup-data.json';
+	import EdgeFade from './EdgeFade.svelte';
+	const countries = feature(data, data.objects.countries);
+
+	const springOptions = { stiffness: 0.04 };
+	const yaw = spring(0, springOptions);
+	const pitch = spring(0, springOptions);
+	const roll = spring(0, springOptions);
+
+	const projection = geoOrthographic().rotate([yaw, pitch, roll]).precision(0.1).clipAngle(90);
+	let sensitivity = 75;
+	let zoom;
+	let scale = 0;
+
+	let selectedFeature: any;
+
+	$: if (selectedFeature) {
+		const centroid = geoCentroid(selectedFeature);
+		$yaw = -centroid[0];
+		$pitch = -centroid[1];
+	}
+
+	const rScale = scaleSqrt()
+		.domain([0, max(meetupData.map((d) => d.total_events))])
+		.range([5, 8]);
+</script>
+
+<div class="h-[700px] flex relative w-[900px]">
+	<Chart
+		geo={{
+			projection: geoOrthographic,
+			fitGeojson: countries,
+			rotate: {
+				yaw: $yaw,
+				pitch: $pitch,
+				roll: $roll
+			},
+			_scale: scale
+		}}
+		tooltip={{ mode: 'manual' }}
+		let:tooltip
+		let:projection
+	>
+		<Svg>
+			<defs>
+				<!-- Define radial gradient -->
+				<radialGradient id="glowGradient" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
+					<stop offset="0%" style="stop-color: white; stop-opacity: 1" />
+					<stop offset="100%" style="stop-color: blue; stop-opacity: 0" />
+				</radialGradient>
+
+				<!-- Define glow filter -->
+				<filter id="glow">
+					<feGaussianBlur stdDeviation="5.5" result="coloredBlur" />
+					<feMerge>
+						<feMergeNode in="coloredBlur" />
+						<feMergeNode in="SourceGraphic" />
+					</feMerge>
+				</filter>
+			</defs>
+
+			<!-- <circle r="480" cx={480} cy={580} fill="url(#glowGradient)" filter="url(#glow)" /> -->
+
+			<Zoom
+				mode="manual"
+				bind:this={zoom}
+				scroll="none"
+				tweened={{ duration: 800, easing: cubicOut }}
+				let:zoomTo
+				let:reset={resetZoom}
+				on:zoom={(e) => {
+					const scale = 250;
+					$yaw = e.detail.translate.x * (sensitivity / scale);
+					$pitch = -e.detail.translate.y * (sensitivity / scale);
+					projection.rotate([yaw, pitch]);
+				}}
+			>
+				<GeoPath
+					geojson={{ type: 'Sphere' }}
+					class="fill-[#dadada] stroke-purple-50"
+					on:click={() => ($yaw = 0)}
+				/>
+
+				<Graticule class="stroke-black/5" />
+				{#each countries.features as country}
+					<GeoPath
+						geojson={country}
+						class={cls(
+							'fill-[#f2f2f2] stroke-[#A5A5A5]/40 cursor-pointer',
+							selectedFeature === country ? 'fill-red-400' : 'hover:fill-gray-200'
+						)}
+						{tooltip}
+					/>
+				{/each}
+				<g class="points pointer-events-none">
+					{#each meetupData as meetup}
+						<EdgeFade latitude={meetup.latitude} longitude={meetup.longitude} geo={projection}>
+							<GeoPoint lat={meetup.latitude} long={meetup.longitude}>
+								<circle r={rScale(meetup.total_events)} class="fill-[#88398A] stroke-[#642965]" />
+							</GeoPoint>
+						</EdgeFade>
+					{/each}
+				</g>
+			</Zoom>
+		</Svg>
+
+		<Tooltip>
+			<div slot="header" let:data>{data.properties.name}</div>
+		</Tooltip>
+	</Chart>
+</div>
--- 2023/week-47-nov-2023-r-ladies/website/src/routes/+page.svelte
+++ 2023/week-47-nov-2023-r-ladies/website/src/routes/+page.svelte
@@ -1,120 +1,15 @@
-<script lang="ts">
-	import { cubicOut } from 'svelte/easing';
-	import { spring } from 'svelte/motion';
-	import { geoOrthographic, geoCentroid, geoDistance } from 'd3-geo';
-	import { feature } from 'topojson-client';
-	import { Chart, Svg, GeoPath, Graticule, Tooltip, Zoom, GeoPoint } from 'layerchart';
-	import { cls, sortFunc, Button, scrollIntoView } from 'svelte-ux';
-	import { scaleSqrt, max, scaleLinear } from 'd3';
-	import data from './countries-110m.json';
-	import meetupData from './meetup-data.json';
-	import EdgeFade from './EdgeFade.svelte';
-	const countries = feature(data, data.objects.countries);
-
-	const springOptions = { stiffness: 0.04 };
-	const yaw = spring(0, springOptions);
-	const pitch = spring(0, springOptions);
-	const roll = spring(0, springOptions);
-
-	const projection = geoOrthographic().rotate([yaw, pitch, roll]).precision(0.1).clipAngle(90);
-
-	let sensitivity = 75;
-	let zoom;
-	let scale = 0;
-
-	let selectedFeature: any;
-
-	$: if (selectedFeature) {
-		const centroid = geoCentroid(selectedFeature);
-		$yaw = -centroid[0];
-		$pitch = -centroid[1];
-	}
-
-	const rScale = scaleSqrt()
-		.domain([0, max(meetupData.map((d) => d.total_events))])
-		.range([3, 12]);
-
-	let pointData = meetupData;
+<script>
+	import Globe from '$lib/components/Globe.svelte';
 </script>
 
-<div class="h-[700px] flex relative">
-	<div class="overflow-y-scroll overflow-x-hidden scrollbar-none">
-		{#each countries.features.sort(sortFunc('properties.name')) as country}
-			{@const isSelected = selectedFeature === country}
-			<div use:scrollIntoView={{ condition: isSelected }}>
-				<Button
-					variant={isSelected ? 'fill-light' : 'default'}
-					color={isSelected ? 'red' : 'default'}
-					class="font-light justify-start"
-					fullWidth
-					on:click={() => (selectedFeature = country)}
-				>
-					{country.properties.name}
-				</Button>
-			</div>
-		{/each}
-	</div>
-	<Chart
-		geo={{
-			projection: geoOrthographic,
-			fitGeojson: countries,
-			rotate: {
-				yaw: $yaw,
-				pitch: $pitch,
-				roll: $roll
-			},
-			_scale: scale
-		}}
-		tooltip={{ mode: 'manual' }}
-		let:tooltip
-		let:projection
-	>
-		<Svg>
-			<Zoom
-				mode="manual"
-				bind:this={zoom}
-				scroll="none"
-				tweened={{ duration: 800, easing: cubicOut }}
-				let:zoomTo
-				let:reset={resetZoom}
-				on:zoom={(e) => {
-					const scale = 650;
-					$yaw = e.detail.translate.x * (sensitivity / scale);
-					$pitch = -e.detail.translate.y * (sensitivity / scale);
-					projection.rotate([yaw, pitch]);
-				}}
-			>
-				<GeoPath
-					geojson={{ type: 'Sphere' }}
-					class="fill-[#dadada] stroke-purple-50"
-					on:click={() => ($yaw = 0)}
-				/>
-
-				<Graticule class="stroke-black/5" />
-				{#each countries.features as country}
-					<GeoPath
-						geojson={country}
-						class={cls(
-							'fill-[#f2f2f2] stroke-[#A5A5A5]/40 cursor-pointer',
-							selectedFeature === country ? 'fill-red-400' : 'hover:fill-gray-200'
-						)}
-						{tooltip}
-					/>
-				{/each}
-				<g class="points pointer-events-none">
-					{#each pointData as meetup}
-						<EdgeFade latitude={meetup.latitude} longitude={meetup.longitude} geo={projection}>
-							<GeoPoint lat={meetup.latitude} long={meetup.longitude}>
-								<circle r={rScale(meetup.total_events)} class="fill-[#88398A] stroke-[#642965]" />
-							</GeoPoint>
-						</EdgeFade>
-					{/each}
-				</g>
-			</Zoom>
-		</Svg>
-
-		<Tooltip>
-			<div slot="header" let:data>{data.properties.name}</div>
-		</Tooltip>
-	</Chart>
-</div>
+<main class="flex justify-between items-center">
+	<section class="flex flex-col justify-center">
+		<div class="flex items-center justify-center gap-4">
+			<h1 class="text-6xl font-bold flex-grow-0">R Ladies</h1>
+			<p>This is some text about what Rladies is and what not</p>
+		</div>
+	</section>
+	<section class="globe">
+		<Globe />
+	</section>
+</main>
